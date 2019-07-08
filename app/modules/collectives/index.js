315d892f36543ffb668d4b831bb713d22ce5c4d2
mp.dev.stack <medmed91@gmail.com>
2019-07-08 23:59:06 +0200

    feat(collective): review css

--- app/modules/collectives/index.js
+++ app/modules/collectives/index.js
@@ -6,55 +6,52 @@ import Content from "../../containers/content";
 const Collectives = () => (
   <Content>
     <Grid className={"page-collectivite"}>
-      <Cell size={12}>
-        <Grid>
-          <Cell size={12}>
+      <Grid>
+        <Cell size={12} className={"illustration-1"}>
+          <div className={"container-illustration"}>
             <img
               src={"./static/conceptCollectivité1.jpg"}
               alt={"conceptCollectivité1"}
-              className={"conceptCollectivité1 zoom"}
+              className={"illustration zoom"}
             />
-          </Cell>
-        </Grid>
-        <Grid className={"section-1"}>
-          <Cell size={6} className={"section-1-1"}>
-            <h3>
-              Nous sommes le partenaire des aménageurs et des collectivités,
-              gestonnaires de la signalisation des commerces, des services, des
-              entreprises artisanales et industrielles
-            </h3>
-            <h2>
-              Notre concept est gratuit pour la commune et autofinancé par les
-              acteurs economiques
-            </h2>
-          </Cell>
-          <Cell size={6} className={"section-1-2"}>
-            <h2>{"ZONE D'ACTIVITES"}</h2>
-            <h2>{"ZONE COMMERCIALE"}</h2>
-            <h2>{"CENTRE VILLE"}</h2>
-          </Cell>
-        </Grid>
-        <Grid>
-          <Cell size={12}>
-            <h3>
-              Nous étudions vos projets pour vous presenter la solution adaptée
-              à vos besoins
-            </h3>
-            <h3>
-              Projet de convention - Etude d&quotimplantation - Entretien
-              régulier de tous les mobiliers rattachés à la convention
-            </h3>
-          </Cell>
-        </Grid>
-        <Grid>
-          <Cell size={12}>
-            <img
-              src={"./static/conceptCollectivité1.jpg"}
-              alt={"conceptCollectivité"}
-              className={"conceptCollectivité1 full"}
-            />
-          </Cell>
-        </Grid>
+          </div>
+        </Cell>
+      </Grid>
+      <Cell size={6} className={"section-1-1 paragraph"}>
+        <p>
+          Nous sommes le partenaire des aménageurs et des collectivités,
+          gestonnaires de la signalisation des commerces, des services, des
+          entreprises artisanales et industrielles
+        </p>
+        <h3 className={"text-color-theme"}>
+          Notre concept est gratuit pour la commune et autofinancé par les
+          acteurs economiques
+        </h3>
+      </Cell>
+      <Cell size={6} className={"section-1-2"}>
+        <h2>{"ZONE D'ACTIVITES"}</h2>
+        <h2>{"ZONE COMMERCIALE"}</h2>
+        <h2>{"CENTRE VILLE"}</h2>
+      </Cell>
+      <Cell size={12} className={"section-2 paragraph"}>
+        <p>
+          Nous étudions vos projets pour vous presenter la solution adaptée à
+          vos besoins
+        </p>
+        <br />
+        <p>
+          Projet de convention - Etude d&#39;implantation - Entretien régulier
+          de tous les mobiliers rattachés à la convention
+        </p>
+      </Cell>
+      <Cell size={12} className={"illustration-2"}>
+        <div className={"container-illustration"}>
+          <img
+            src={"./static/conceptCollectivité1.jpg"}
+            alt={"conceptCollectivité"}
+            className={"illustration zoom"}
+          />
+        </div>
       </Cell>
     </Grid>
   </Content>
